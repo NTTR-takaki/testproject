4b3c4bce1f5645156a44194b73ea7698fd728fb0
NTTR-takaki <takaki@nttr.co.jp>
2017-08-01 17:39:12 +0900

    初回コミット

---
+++ testprojectZ/testprojectZ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testprojectZ
+//
+//  Created by mypocket on 2017/08/01.
+//  Copyright © 2017年 mypocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testprojectZ/testprojectZ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testprojectZ
+//
+//  Created by mypocket on 2017/08/01.
+//  Copyright © 2017年 mypocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
